5a820a9e2c3c0d00b08aa49eb325c46d3f51a21f
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-03-26 19:50:36 +0100

    celan start of branch

--- myOS1/to-work on/video.h
+++
@@ -1,3 +0,0 @@
-#pragma once 
-void video_init(void);
-//pokud tahgle funkce nebude fungovat tak se lowk omlouvam hell yeaaaah
--- nolib_projects/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-gcc -nostdlib -o logic logic.c
-
-
-echo "Build complete. Output file: logic"
